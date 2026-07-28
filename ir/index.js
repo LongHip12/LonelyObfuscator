615d9ef193d4de105051aab397e14f3ad23f0ffd
LongHip12 <longhip2012@gmail.com>
2026-07-28 22:21:40 +0700

    Delete ir/index.js

--- ir/index.js
+++
@@ -1,7 +0,0 @@
-const Opcode = require('./Opcode');
-const Enums = require('./Enums');
-const Constant = require('./Constant');
-const Instruction = require('./Instruction');
-const Chunk = require('./Chunk');
-
-module.exports = { Opcode, Enums, Constant, Instruction, Chunk };
