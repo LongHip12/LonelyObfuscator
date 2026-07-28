697d9beed934e9415a1042135dd2abd498e37ef5
LongHip12 <longhip2012@gmail.com>
2026-07-28 22:21:34 +0700

    Delete ir/Enums.js

--- ir/Enums.js
+++
@@ -1,5 +0,0 @@
-const ConstantType = { Nil: 0, Boolean: 1, Number: 2, String: 3 };
-const InstructionType = { ABC: 0, ABx: 1, AsBx: 2, AsBxC: 3, Data: 4 };
-const InstructionConstantMask = { NK: 0, RA: 1, RB: 2, RC: 4 };
-
-module.exports = { ConstantType, InstructionType, InstructionConstantMask };
