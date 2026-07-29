bcb5f4a1ae0eb8a08cd62645c13bdda13f89780f
LongHip12 <longhip2012@gmail.com>
2026-07-29 13:22:54 +0700

    Upload ir/index.js

---
+++ ir/index.js
@@ -0,0 +1,7 @@
+const Opcode = require('./Opcode');
+const Enums = require('./Enums');
+const Constant = require('./Constant');
+const Instruction = require('./Instruction');
+const Chunk = require('./Chunk');
+
+module.exports = { Opcode, Enums, Constant, Instruction, Chunk };
