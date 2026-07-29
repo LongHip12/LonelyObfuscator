20e1d101d6607f56cea94f776374b4176cedb8fc
LongHip12 <longhip2012@gmail.com>
2026-07-29 13:21:32 +0700

    Upload obfuscator/opcodes/OpNe.js

---
+++ obfuscator/opcodes/OpNe.js
@@ -0,0 +1,69 @@
+const Opcode = require('../../ir/Opcode');
+const { InstructionType, InstructionConstantMask } = require('../../ir/Enums');
+
+class OpNe {
+  constructor() { this.VIndex = 0; }
+  isInstruction(instruction) {
+    return instruction.opCode === Opcode.Eq && instruction.A !== 0 && instruction.B <= 255 && instruction.C <= 255;
+  }
+  getObfuscated() {
+    return "if(Stk[Inst[OP_A]]~=Stk[Inst[OP_C]])then InstrPoint=InstrPoint+1;else InstrPoint=Inst[OP_B];end;";
+  }
+  mutate(instruction) {
+    instruction.A = instruction.B;
+    instruction.B = instruction.pc + instruction.chunk.instructions[instruction.pc + 1].B + 2;
+    instruction.instructionType = InstructionType.AsBxC;
+  }
+}
+
+class OpNeB {
+  constructor() { this.VIndex = 0; }
+  isInstruction(instruction) {
+    return instruction.opCode === Opcode.Eq && instruction.A !== 0 && instruction.B > 255 && instruction.C <= 255;
+  }
+  getObfuscated() {
+    return "if(Inst[OP_A] ~= Stk[Inst[OP_C]]) then InstrPoint=InstrPoint+1;else InstrPoint=Inst[OP_B];end;";
+  }
+  mutate(instruction) {
+    instruction.A = instruction.B - 255;
+    instruction.B = instruction.pc + instruction.chunk.instructions[instruction.pc + 1].B + 2;
+    instruction.instructionType = InstructionType.AsBxC;
+    instruction.constantMask |= InstructionConstantMask.RA;
+  }
+}
+
+class OpNeC {
+  constructor() { this.VIndex = 0; }
+  isInstruction(instruction) {
+    return instruction.opCode === Opcode.Eq && instruction.A !== 0 && instruction.B <= 255 && instruction.C > 255;
+  }
+  getObfuscated() {
+    return "if(Stk[Inst[OP_A]] ~= Inst[OP_C]) then InstrPoint=InstrPoint+1;else InstrPoint=Inst[OP_B];end;";
+  }
+  mutate(instruction) {
+    instruction.A = instruction.B;
+    instruction.C -= 255;
+    instruction.B = instruction.pc + instruction.chunk.instructions[instruction.pc + 1].B + 2;
+    instruction.instructionType = InstructionType.AsBxC;
+    instruction.constantMask |= InstructionConstantMask.RC;
+  }
+}
+
+class OpNeBC {
+  constructor() { this.VIndex = 0; }
+  isInstruction(instruction) {
+    return instruction.opCode === Opcode.Eq && instruction.A !== 0 && instruction.B > 255 && instruction.C > 255;
+  }
+  getObfuscated() {
+    return "if(Inst[OP_A] ~= Inst[OP_C])then InstrPoint=InstrPoint+1;else InstrPoint=Inst[OP_B];end;";
+  }
+  mutate(instruction) {
+    instruction.A = instruction.B - 255;
+    instruction.C -= 255;
+    instruction.B = instruction.pc + instruction.chunk.instructions[instruction.pc + 1].B + 2;
+    instruction.instructionType = InstructionType.AsBxC;
+    instruction.constantMask |= InstructionConstantMask.RA | InstructionConstantMask.RC;
+  }
+}
+
+module.exports = { OpNe, OpNeB, OpNeC, OpNeBC };
